912ac460096a0576c2c27e79618814af14362507
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-09-24 00:19:42 +0900

    userモデルバリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 英数字混合6文字以上の正規表現
+  VALID_PASSWORD_REGIX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/
+  validates :password, format: { with: VALID_PASSWORD_REGIX }
+
+  with_options presence: true do
+    validates :nickname
+    validates :full_name
+    validates :birthday
+    validates :full_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
