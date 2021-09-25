8a408a6f112fde709dfdc8c41b72d26e6b211fc2
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-09-25 19:55:16 +0900

    マイページ編集画面のフロント実装など

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   # 英数字混合6文字以上の正規表現
   VALID_PASSWORD_REGIX = /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/
-  validates :password, format: { with: VALID_PASSWORD_REGIX }
+  validates :password, format: { with: VALID_PASSWORD_REGIX }, on: :create
 
   with_options presence: true do
     validates :nickname
