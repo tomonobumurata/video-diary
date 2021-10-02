eed932e952a72268dea67988be170f7ceb63072e
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-10-02 18:32:47 +0900

    reviewモデル バリデーションの記述

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :genre_id
+    validates :star_id
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :genre_id
+    validates :star_id
+    validates :place_id
+  end
 end
