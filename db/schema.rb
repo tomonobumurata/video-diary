45be58349aa32ce9642cacd98762f5a1d5d2f4cb
tomonobu <81632360+tomonobumurata@users.noreply.github.com>
2021-10-02 17:46:25 +0900

    reviewモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_050224) do
+ActiveRecord::Schema.define(version: 2021_10_02_082555) do
+
+  create_table "reviews", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.date "date"
+    t.integer "genre_id", null: false
+    t.integer "star_id", null: false
+    t.integer "place_id"
+    t.text "impression"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_09_18_050224) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reviews", "users"
 end
